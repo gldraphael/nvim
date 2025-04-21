18b515a9a91f1199a6547fac923510760b7292bc
Galdin Raphael <gldraphael@users.noreply.github.com>
2025-04-21 21:57:19 +0100

    WSL: add system clipboard support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,28 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Some OS detectors
+local is_wsl = vim.fn.has("wsl") == 1
+-- local is_mac = vim.fn.has("macunix") == 1
+-- local is_linux = not is_wsl and not is_mac
+
+-- Show absolute line numbers
 vim.opt.relativenumber = false
+
+-- WSL Clipboard support
+if is_wsl then
+  -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
+  -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
